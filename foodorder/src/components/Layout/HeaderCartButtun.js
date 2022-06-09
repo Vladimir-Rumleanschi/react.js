0f4bcea5160ab1dc899b554c5ca67ef099501f92
Vladimir Rumleanschi <vrumleanschi@gmail.com>
2022-06-09 19:29:18 +0200

    Add amount to the cart method with ref.

--- foodorder/src/components/Layout/HeaderCartButtun.js
+++ foodorder/src/components/Layout/HeaderCartButtun.js
@@ -1,14 +1,24 @@
+import {useContext} from 'react';
 
+import CartContext from "../../store/cart-contex";
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButtun.module.css';
+import cartContex from "../../store/cart-contex";
 
 const HeaderCartButtun = props =>{
-    return <button className={classes.button}>
+
+    const cartCtx = useContext(cartContex);
+    const numberOfCartItems = cartCtx.items.reduce((curNumber,item) =>{
+        return curNumber + item.amount
+    },0);
+
+
+    return <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
 };
 
