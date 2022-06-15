544d4615ff7446c6b95501fa544d9700d39580ff
Vladimir Rumleanschi <vrumleanschi@gmail.com>
2022-06-15 13:20:16 +0200

    Fetch data then the page is up .

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState ,useEffect,useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,9 @@ function App() {
   const [isLoading,setIsLoading] = useState(false);
   const [error,setError] = useState(null);
 
-  async function fetschMoviesHandler(){
+
+
+  const fetschMoviesHandler = useCallback(async () =>{
     setIsLoading(true);
     setError(null);
 
@@ -35,7 +37,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  },[]);
+
+  useEffect(() =>{
+    fetschMoviesHandler();
+  },[fetschMoviesHandler]);
 
   let content = <p>Found no movies.</p>
 
