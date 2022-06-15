e450756b5bb8ae46b019039b573bfb81b75b20c0
Vladimir Rumleanschi <vrumleanschi@gmail.com>
2022-06-15 12:23:02 +0200

    Adding 'asyns' and 'await' for a cleaner http request

--- backend/src/App.js
+++ backend/src/App.js
@@ -7,20 +7,19 @@ function App() {
 
   const [movies,setMovies] = useState([]);
 
-  function fetschMoviesHandler(){
-    fetch('https://swapi.dev/api/films').then(response =>{
-       return response.json();
-    }).then(data =>{
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      });
-      setMovies(transformedMovies);
+  async function fetschMoviesHandler(){
+    const response = await fetch('https://swapi.dev/api/films')
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
     });
+    setMovies(transformedMovies);
+
   }
 
   return (
