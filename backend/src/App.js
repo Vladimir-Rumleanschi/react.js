f0e8960f279966cac219349582c0e7a78da127bc
Vladimir Rumleanschi <vrumleanschi@gmail.com>
2022-06-15 12:31:12 +0200

    Handling loading till the http request is done.

--- backend/src/App.js
+++ backend/src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 function App() {
 
   const [movies,setMovies] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
 
   async function fetschMoviesHandler(){
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films')
     const data = await response.json();
     const transformedMovies = data.results.map(movieData => {
@@ -19,7 +21,7 @@ function App() {
       }
     });
     setMovies(transformedMovies);
-
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +30,9 @@ function App() {
         <button onClick={fetschMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies}/>}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p> }
+        {isLoading && <p>Is Loading ...</p>}
       </section>
     </React.Fragment>
   );
