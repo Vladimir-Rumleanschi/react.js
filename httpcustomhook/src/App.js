79f0b0374bf16fc5436c1b8cd54b9754fa16861b
Vladimir Rumleanschi <vrumleanschi@gmail.com>
2022-06-16 17:56:10 +0200

    http hook

--- httpcustomhook/src/App.js
+++ httpcustomhook/src/App.js
@@ -1,43 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useCallback } from 'react';
 
+import useHttp from "./hooks/use-http";
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
+
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
   const [tasks, setTasks] = useState([]);
+  
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-3429d-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-      );
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  const {isLoading,error, sendRequest:fetchTasks} = useHttp();
 
-      const data = await response.json();
-
+  useEffect(() => {
+    const transformTask = taskObj => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    };
+    fetchTasks( {url: 'https://react-http-3429d-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'},
+        transformTask
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
